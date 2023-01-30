bdf1cd7d2ef4345637fc5a19b11a3127ec5707f1
Nico Desktop <diaznicox96@gmail.com>
2023-01-29 22:39:39 -0300

    svg as collapse button added, now working 100%

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-import { colapsar } from "./functions.js";
+import { colapsar, svg_colapsar } from "./functions.js";
 
 /* Referencia de contenedores */
 const $contenedor_educacion = document.querySelector("#containerEducationItems");
@@ -6,13 +6,25 @@ const $contenedor_trabajo = document.querySelector("#containerWorkItems");
 const $contenedor_habilidades = document.querySelector("#containerAbilityItems");
 const $contenedor_proyectos = document.querySelector("#containerProyects");
 
+/* Referencia de contenedores del boton de colapsar/expandir */
+const $contenedor_btn_educacion = document.querySelector("#containerEducationButton");
+const $contenedor_btn_trabajo = document.querySelector("#containerWorkButton");
+const $contenedor_btn_habilidades = document.querySelector("#containerAbilitiesButton");
+const $contenedor_btn_proyectos = document.querySelector("#containerProyectsButton");
+
+// ----> Al contenedor del texto de cada sección le agrego el archivo SVG de manera dinámica
+$contenedor_btn_educacion.appendChild(svg_colapsar("btnCollapseEducation"));
+$contenedor_btn_trabajo.appendChild(svg_colapsar("btnCollapseWork"));
+$contenedor_btn_habilidades.appendChild(svg_colapsar("btnCollapseAbilities"));
+$contenedor_btn_proyectos.appendChild(svg_colapsar("btnCollapseProyects"));
+
 /* Referencia de botones */
 const $boton_colapsar_educacion = document.querySelector("#btnCollapseEducation");
 const $boton_colapsar_trabajo = document.querySelector("#btnCollapseWork");
 const $boton_colapsar_habilidades = document.querySelector("#btnCollapseAbilities");
 const $boton_colapsar_proyectos = document.querySelector("#btnCollapseProyects");
 
-colapsar($boton_colapsar_educacion, $contenedor_educacion);
-colapsar($boton_colapsar_trabajo, $contenedor_trabajo);
-colapsar($boton_colapsar_habilidades, $contenedor_habilidades);
-colapsar($boton_colapsar_proyectos, $contenedor_proyectos);
+colapsar($boton_colapsar_educacion, $contenedor_educacion, $contenedor_btn_educacion);
+colapsar($boton_colapsar_trabajo, $contenedor_trabajo, $contenedor_btn_trabajo);
+colapsar($boton_colapsar_habilidades, $contenedor_habilidades, $contenedor_btn_habilidades);
+colapsar($boton_colapsar_proyectos, $contenedor_proyectos, $contenedor_btn_proyectos);
--- js/functions.js
+++ js/functions.js
@@ -1,13 +1,77 @@
-const colapsar = (refBoton, refContenedor) => {
-    if (typeof refBoton != "null" && typeof refContenedor != "null"){
-        refBoton.addEventListener("click", (e) => {
-            if (refContenedor.classList.contains("visually-hidden")){
-                refContenedor.classList.toggle("visually-hidden");
-            } else{
-                refContenedor.classList.toggle("visually-hidden");
-            }
-        });
-    }
+/*
+    Función que colapsa o retrae el contenedor de items de cada sección.
+    Recibe por parámetro las referencias del contenedor a expandir/colapsar
+    y el botón cuyo evento manejará la acción.
+*/
+const colapsar = (refBoton, refContenedorItems) => {
+    refBoton.addEventListener("click", () => {
+        if (refContenedorItems.classList.contains("visually-hidden")){
+            refContenedorItems.classList.remove("visually-hidden");
+            change_status_svg(refBoton);
+        } else{
+            refContenedorItems.classList.add("visually-hidden");
+            change_status_svg(refBoton);
+        }
+    });
 };
 
-export { colapsar };
+/*
+    Función que cambia el estilo del elemento SVG de manera dinámica, en este caso verifica si el SVG
+    contiene en total 4 elementos hijos (si es así es porque el elemento SVG es
+    cuadro con el símbolo '+' en su interior), y lo elimina para que sea un '-'.
+    Caso contrario agrega la línea que falta para que vuelva a ser un '+'.
+*/
+const change_status_svg = (elementSVG) => {
+    if (elementSVG.childElementCount == 4){
+        elementSVG.removeChild(elementSVG.childNodes[3]);
+    } else{
+        const line2 = createElementCustom("line", {"x1":"12", "y1":"9", "x2":"12", "y2":"15"});
+        elementSVG.appendChild(line2);
+    }
+}
+
+/*
+    Creo un elemento SVG de manera dinámica. En este caso utilizo un SVG que es un
+    cuadro con el símbolo '-' en su interior. Por eso se genera con sus respectivos
+    atributos y elementos.
+*/
+const svg_colapsar = (id) => {
+    const svg = createElementCustom("svg",
+        {"id":id, 
+        "width":"44", 
+        "height":"44", 
+        "viewBox":"0 0 24 24", 
+        "stroke-width":"1.5", 
+        "stroke":"#000000",
+        "fill":"none",
+        "stroke-linecap":"round",
+        "stroke-linejoin":"round"
+    });
+    svg.classList.add(...["icon", "icon-tabler", "icon-tabler-square-minus", "btn-contraer"]);
+    // Creo sus dependencias de la misma manera y los uno al svg principal
+    const path = createElementCustom("path", {"stroke":"none","d":"M0 0h24v24H0z", "fill":"none"});
+    const rect = createElementCustom("rect", {"x":"4","y":"4","width":"16", "height":"16", "rx":"2"});
+    const line = createElementCustom("line", {"x1":"9", "y1":"12","x2":"15","y2":"12"});
+    svg.appendChild(path);
+    svg.appendChild(rect);
+    svg.appendChild(line);
+    return svg;
+}
+
+/*
+    Función original hecha por mí. Crea un elemento customizado para SVG.
+    Recibe por parámetro el tipo de elemento ('svg', 'rect', 'line', etc), y
+    los atributos en forma de objeto para ir agregándolos de manera ordenada.
+*/
+
+function createElementCustom(tipo, atributos){
+    const newElementSVG = document.createElementNS('http://www.w3.org/2000/svg', tipo);
+    if (typeof(atributos) == "object"){
+        Object.keys(atributos).forEach((atributo) => {
+            newElementSVG.setAttribute(atributo, atributos[atributo]);
+        })
+    }
+    return newElementSVG;
+}
+
+export { colapsar, svg_colapsar };
